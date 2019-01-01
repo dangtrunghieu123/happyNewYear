e09c2d326c740a27a713f112a6aa85ce42164740
thulee <lethu210398@gmail.com>
2019-01-01 16:55:40 +0700

    happy new year NPD

---
+++ javascript.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    console.log($('#test').length);
+    // cusor = '+';
+    // count = 0;
+    // mess = 'Happyy New Year to Nguyen Phuoc Dai '
+
+    // if($('#test').length>0){
+    //     console.log($('.test'));
+    // }
+    // function type() {
+        
+    //     if (cusor == '+') cusor = ' '; else cusor = '+';
+    //     $('#test').html(mess.substring(0, count++) + cusor);
+    //     if (count <= mess.length) setTimeout("type()", 80);
+    // }
+});
